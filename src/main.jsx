0653e6dba0a0214de573d0a33404dcde2ae992e7
kimi.casamina <kimi.casamina@gmail.com>
2024-12-06 17:25:17 +0800

    frontend

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { ProvideAuth } from "./context/auth.jsx";
+import { Toaster } from "react-hot-toast";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <ProvideAuth>
+      <Toaster position="bottom-center" reverseOrder={false} />
+      <App />
+    </ProvideAuth>
+  </StrictMode>
+);
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,35 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { config } from "dotenv";
+import { resolve } from "path";
+import fs from "fs/promises";
+
+config();
 
 // https://vite.dev/config/
 export default defineConfig({
+  define: {
+    "process.env": process.env,
+  },
+  // --- this config enables jsx at js constant
+  resolve: {
+    alias: {
+      src: resolve(__dirname, "src"),
+    },
+  },
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      "/api": {
+        target:
+          process.env.VITE_MODE === "production"
+            ? process.env.VITE_PASSTHESALT_SERVER
+            : "http://localhost:9000",
+      },
+
+      "/admin": {
+        target: "http://localhost:8081/",
+      },
+    },
+  },
+});
